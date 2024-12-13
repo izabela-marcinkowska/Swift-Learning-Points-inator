407c67965dcf6a2fcb6fe82c605ce31bf3cabce2
Devy <izabela.marcinkowska@outlook.com>
2024-12-13 10:19:08 +0100

    first commit

---
+++ Swift-Learning-Points-inator/Swift_Learning_Points_inatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Learning_Points_inatorApp.swift
+//  Swift-Learning-Points-inator
+//
+//  Created by Izabela Marcinkowska on 2024-12-13.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Learning_Points_inatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
