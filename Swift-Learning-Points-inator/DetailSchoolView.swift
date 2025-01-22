9975dfae2235f1c4a7d5f72f8fb12b8cc34da025
Devy <izabela.marcinkowska@outlook.com>
2025-01-22 10:19:54 +0100

    feat: Show task list on every school detail view

--- Swift-Learning-Points-inator/DetailSchoolView.swift
+++ Swift-Learning-Points-inator/DetailSchoolView.swift
@@ -6,36 +6,56 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct DetailSchoolView: View {
     let school: SchoolOfMagic
-    var body: some View {
-        VStack(spacing: 20) {
-            // Header section
-            Image(systemName: school.icon)
-                .font(.system(size: 60))
-                .foregroundColor(.blue)  // Or any color you prefer
-            
-            Text(school.rawValue)
-                .font(.title)
-                .bold()
-            
-            // Description section
-            VStack(alignment: .leading, spacing: 10) {
-                Text("Description")
-                    .font(.headline)
-                
-                Text(school.description)
-                    .multilineTextAlignment(.leading)
-            }
-            .padding()
-            .frame(maxWidth: .infinity, alignment: .leading)
-            
-            Spacer()
-        }
-        .padding()
-        .navigationTitle(school.rawValue)
+    @Query private var tasks: [Task]
+    
+    private var uncompletedTasks: [Task] {
+        tasks.filter {$0.school == school && !$0.isCompleted}
     }
+    
+    private var completedTasks: [Task] {
+        tasks.filter {$0.school == school && $0.isCompleted}
+    }
+    
+    var body: some View {
+            List {
+                // Header Section
+                Section {
+                    VStack(alignment: .center, spacing: 20) {
+                        Image(systemName: school.icon)
+                            .font(.system(size: 60))
+                            .foregroundColor(.blue)
+                        
+                        Text(school.description)
+                            .multilineTextAlignment(.leading)
+                    }
+                    .frame(maxWidth: .infinity)
+                    .padding(.vertical)
+                }
+                
+                // Uncompleted Tasks Section
+                if !uncompletedTasks.isEmpty {
+                    Section("Uncompleted Tasks") {
+                        ForEach(uncompletedTasks) { task in
+                            TaskRowView(task: task)
+                        }
+                    }
+                }
+                
+                // Completed Tasks Section
+                if !completedTasks.isEmpty {
+                    Section("Completed Tasks") {
+                        ForEach(completedTasks) { task in
+                            TaskRowView(task: task)
+                        }
+                    }
+                }
+            }
+            .navigationTitle(school.rawValue)
+        }
 }
 #Preview {
     NavigationStack {
