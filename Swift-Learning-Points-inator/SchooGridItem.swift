c92f856c4f7a490f2839128fc0bc877f81cf5e15
Devy <izabela.marcinkowska@outlook.com>
2025-01-22 13:01:50 +0100

    feat: Create Grid Item with short information

---
+++ Swift-Learning-Points-inator/SchooGridItem.swift
@@ -0,0 +1,77 @@
+//
+//  SchooGridItem.swift
+//  Swift-Learning-Points-inator
+//
+//  Created by Izabela Marcinkowska on 2025-01-22.
+//
+
+import SwiftUI
+import SwiftData
+
+struct SchooGridItem: View {
+    let school: SchoolOfMagic
+    @Query private var users: [User]
+    @Environment(\.modelContext) private var modelContext
+    
+    private var user: User? {
+        users.first
+    }
+    
+    private var schoolProgress: SchoolProgress? {
+        user?.schoolProgress.first { $0.school == school}
+    }
+    
+    private var nextLevel: SchoolOfMagic.AchievementLevel? {
+        guard let currentLevel = schoolProgress?.currentLevel,
+              currentLevel.rawValue < SchoolOfMagic.AchievementLevel.allCases.count - 1 else {
+            return nil
+        }
+        return SchoolOfMagic.AchievementLevel(rawValue: currentLevel.rawValue + 1)
+    }
+    
+    private var manaProgress: String {
+        let currentMana = schoolProgress?.totalMana ?? 0
+        let nextThreshold = nextLevel?.manaThreshold ?? currentMana
+        return "\(currentMana)/\(nextThreshold)"
+    }
+    
+    var body: some View {
+        VStack(alignment: .center, spacing: 16) {
+            Image(systemName: school.icon)
+                .font(.system(size: 32))
+                .foregroundColor(.blue)
+                .padding(.top, 8)
+            
+            VStack(spacing: 8) {
+                
+            
+            Text(school.rawValue)
+                .font(.headline)
+                .multilineTextAlignment(.center)
+                .lineLimit(2)
+                .fixedSize(horizontal: false, vertical: true)
+                .frame(maxWidth: .infinity)
+            
+            Text(school.titleForLevel(schoolProgress?.currentLevel ?? .apprentice))
+                .font(.subheadline)
+                .multilineTextAlignment(.center)
+                .lineLimit(2)
+                .fixedSize(horizontal: false, vertical: true)
+                .frame(maxWidth: .infinity)
+            }
+            .padding(.horizontal, 8)
+            
+            Text(manaProgress)
+                .font(.caption)
+                .padding(.bottom, 8)
+        }
+        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.gray.opacity(0.1))
+        .cornerRadius(10)
+    }
+}
+
+#Preview {
+    SchooGridItem(school: .dataSorcery)
+}
