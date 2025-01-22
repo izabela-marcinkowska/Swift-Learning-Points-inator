12ae23a19b7803936609b2fde79ab7810f3dc201
Devy <izabela.marcinkowska@outlook.com>
2025-01-22 13:02:48 +0100

    feat: Use grid view to show grid items

--- Swift-Learning-Points-inator/SchoolsView.swift
+++ Swift-Learning-Points-inator/SchoolsView.swift
@@ -12,16 +12,24 @@ struct SchoolsView: View {
     @Query private var users: [User]
     private var user: User? { users.first }
     
+    let columns = [
+        GridItem(.adaptive(minimum: 150, maximum: 200), spacing: 16)
+    ]
+    
     var body: some View {
-        List(SchoolOfMagic.allCases, id: \.self) { school in
-            NavigationLink {
-                DetailSchoolView(school: school)
-            } label: {
-            HStack {
-                Image(systemName: school.icon)
-                Text(school.rawValue)
+        ScrollView {
+            LazyVGrid(columns: columns, spacing: 20) {
+                ForEach(SchoolOfMagic.allCases, id: \.self) { school in
+                    NavigationLink {
+                        DetailSchoolView(school: school)
+                    } label: {
+                        SchooGridItem(school: school)
+                            .frame(height: 200)
+                            .foregroundColor(.primary)
+                    }
+                }
             }
-            }
+            .padding()
         }
         .navigationTitle("Schools of Magic")
     }
