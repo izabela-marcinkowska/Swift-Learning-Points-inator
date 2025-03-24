7ca27e8b711143b024a4672e1339cad4e49baf3b
Devy <izabela.marcinkowska@outlook.com>
2025-03-24 23:49:55 +0100

    feat: Add icon possibility form school and spell

--- Swift-Learning-Points-inator/Models/MagicalAlertModel.swift
+++ Swift-Learning-Points-inator/Models/MagicalAlertModel.swift
@@ -8,13 +8,20 @@
 import Foundation
 
 struct MagicalAlertModel {
+    enum AlertIcon {
+        case systemIcon(name: String)
+        case image(name: String)
+        case spellLevel(level: SpellLevel)
+        case schoolIcon(school: SchoolOfMagic)
+    }
+    
     var title: String
     var message: String
     var primaryButtonText: String
     var secondaryButtonText: String?
     var primaryAction: () -> Void
     var secondaryAction: (() -> Void)?
-    var icon: String?
+    var icon: AlertIcon?
     
     static func basic(
         title: String,
@@ -47,6 +54,40 @@ struct MagicalAlertModel {
             secondaryButtonText: cancelText,
             primaryAction: onConfirm,
             secondaryAction: onCancel
+        )
+    }
+    
+    static func levelUp(
+        spellName: String,
+        level: SpellLevel,
+        buttonText: String = "Fantastiskt!",
+        action: @escaping () -> Void = {}
+    ) -> MagicalAlertModel {
+        MagicalAlertModel(
+            title: "Ny nivå uppnådd!",
+            message: "\(spellName) har nu nått \(level.title)!",
+            primaryButtonText: buttonText,
+            secondaryButtonText: nil,
+            primaryAction: action,
+            secondaryAction: nil,
+            icon: .spellLevel(level: level)
+        )
+    }
+    
+    static func schoolProgress(
+        school: SchoolOfMagic,
+        level: SchoolOfMagic.AchievementLevel,
+        buttonText: String = "Fortsätt",
+        action: @escaping () -> Void = {}
+    ) -> MagicalAlertModel {
+        MagicalAlertModel(
+            title: "Skolframgång!",
+            message: "Du har uppnått \(school.titleForLevel(level)) i \(school.rawValue)!",
+            primaryButtonText: buttonText,
+            secondaryButtonText: nil,
+            primaryAction: action,
+            secondaryAction: nil,
+            icon: .schoolIcon(school: school)
         )
     }
     
