caec1682b86ce1b4648ba75934138256b8596709
Devy <izabela.marcinkowska@outlook.com>
2025-04-04 19:21:29 +0200

    feat: Create ombiarding model

---
+++ Swift-Learning-Points-inator/Models/OnboardingManager.swift
@@ -0,0 +1,28 @@
+//
+//  OnboardingManager.swift
+//  Swift-Learning-Points-inator
+//
+//  Created by Izabela Marcinkowska on 2025-04-04.
+//
+
+import Foundation
+
+class OnboardingManager: ObservableObject {
+    @Published var hasCompletedOnboardning: Bool
+    
+    private let hasCompletedOnboardingKey = "hasCompletedOnboarding"
+    
+    init() {
+        self.hasCompletedOnboardning = UserDefaults.standard.bool(forKey: hasCompletedOnboardingKey)
+    }
+    
+    func completeOmboardning() {
+        hasCompletedOnboardning = true
+        UserDefaults.standard.set(true, forKey: hasCompletedOnboardingKey)
+    }
+    
+    func resetOmboarding() {
+        hasCompletedOnboardning = false
+        UserDefaults.standard.set(false, forKey: hasCompletedOnboardingKey)
+    }
+}
