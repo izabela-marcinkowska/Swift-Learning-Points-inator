6d1460ee2a0945dbb7b568327f9de79a4c35acdd
Devy <izabela.marcinkowska@outlook.com>
2025-04-07 09:15:24 +0200

    feat: welcome view

---
+++ Swift-Learning-Points-inator/Views/Onboarding/OnboardingWelcomeView.swift
@@ -0,0 +1,34 @@
+//
+//  OnboardingWelcomeView.swift
+//  Swift-Learning-Points-inator
+//
+//  Created by Izabela Marcinkowska on 2025-04-07.
+//
+
+import SwiftUI
+
+struct OnboardingWelcomeView: View {
+    var body: some View {
+        VStack(spacing: 30) {
+            Image("magic-book")
+                .resizable()
+                .scaledToFit()
+                .frame(height: 180)
+            
+            Text("Welcome to Digital Sorcery!")
+                .font(.title)
+                .fontWeight(.bold)
+                .multilineTextAlignment(.center)
+            
+            Text("Begin your journey into magical coding with this enchanted learning tracker.")
+                .font(.body)
+                .multilineTextAlignment(.center)
+                .padding(.horizontal, 20)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    OnboardingWelcomeView()
+}
